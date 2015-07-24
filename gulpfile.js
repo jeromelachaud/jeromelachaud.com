2f4247f1707ec2836aacd47d94fd185cf9a6ddb6
Jerome Lachaud <jerome.lachaud@gmail.com>
2015-07-24 17:34:50 +0100

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var argv = require('yargs').argv;
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
 var concat = require('gulp-concat');
 var del = require('del');
-var extender = require('gulp-html-extend');
 var size = require('gulp-size');
 var gulpif = require('gulp-if');
 var htmlreplace = require('gulp-html-replace');
-var jshint = require('gulp-jshint');
 var minifyCSS = require('gulp-minify-css');
 var reload = browserSync.reload;
 var rename = require('gulp-rename');
@@ -18,7 +17,9 @@ var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
+var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
+
 
 var basePaths = {
 	src: 'src/',
@@ -28,19 +29,19 @@ var basePaths = {
 var paths = {
 	styles: {
 		src: basePaths.src + 'styles/',
-		dest: basePaths.dest + 'css/'
+		dest: basePaths.dest + 'assets/css/'
 	},
 	scripts: {
 		src: basePaths.src + 'scripts/',
-		dest: basePaths.dest + 'js/'
+		dest: basePaths.dest + '/assets/js/'
 	},
 	images: {
 		src: basePaths.src + 'images/',
-		dest: basePaths.dest + 'img/'
+		dest: basePaths.dest + '/images/'
 	},
 	fonts: {
 		src: basePaths.src + 'fonts/',
-		dest: basePaths.dest + 'css/fonts/'
+		dest: basePaths.dest + 'assets/fonts/'
 	}
 };
 
@@ -53,14 +54,14 @@ gulp.task('html', function () {
 	.pipe(extender({annotations:true,verbose:false})) // default options
 	.pipe(gulpif(argv.production, htmlreplace({
 		'styles_production': 'css/styles.min.css',
-		'js_production': 'js/app.min.js'
+		'js_production': 'js/main.min.js'
 	})))
 	.pipe(gulp.dest((basePaths.dest)));
 });
 
 gulp.task('styles', function () {
 	return gulp.src([
-		paths.styles.src + '**/*.scss',
+		paths.styles.src + '**/*.*',
 	])
 	.pipe(gulpif(argv.dev, sourcemaps.init()))
 	.pipe(sass({errLogToConsole: true}))
@@ -77,17 +78,22 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function () {
 	return gulp.src([
-		paths.scripts.src + 'vendors/jquery-1.11.0.min.js',
-		paths.scripts.src + 'vendors/bootstrap.min.js',
-		paths.scripts.src + '*.js'
+		paths.scripts.src + '**/*.js'
 		])
-	.pipe(jshint('.jshintrc'))
-	.pipe(jshint.reporter('default'))
 	.pipe(size({title: 'scripts'}))
-	.pipe(gulpif(argv.production, concat('app.js')))
+	.pipe(gulpif(argv.production, concat('main.js')))
 	.pipe(gulpif(argv.production, rename({suffix: '.min'})))
 	.pipe(gulpif(argv.production, uglify({preserveComments: 'some'})))// Keep some comments
 	.pipe(gulp.dest((paths.scripts.dest)));
+});
+
+gulp.task('browserify', function() {
+    return browserify('js/main.js')
+        .bundle()
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('images', function () {
@@ -102,7 +108,7 @@ gulp.task('images', function () {
 
 gulp.task('fonts', function() {
 	gulp.src(paths.fonts.src + '**/*.*')
-	.pipe(gulp.dest('build/fonts/'))
+	.pipe(gulp.dest(paths.fonts.dest))
 	.pipe(size({title: 'fonts'}));
 });
 
